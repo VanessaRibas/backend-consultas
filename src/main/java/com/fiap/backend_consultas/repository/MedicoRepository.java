1bbd9fc0fd918598c1d870d04ed372ff336475db
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-23 11:32:55 -0300

    feat: Implementação da entidade medico

---
+++ src/main/java/com/fiap/backend_consultas/repository/MedicoRepository.java
@@ -0,0 +1,5 @@
+package com.fiap.backend_consultas.repository;
+import com.fiap.backend_consultas.model.Medico;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface MedicoRepository extends JpaRepository<Medico,Long>{
+}
