aa136bd050f858fd97bc1a82d69fbfb651bbd3c0
VanessaRibas <vanessairis545@gmail.com>
2026-08-17 10:24:24 -0300

    feat: adiciona serviços para médicos pacientes e especialidades

--- src/main/java/com/fiap/backend_consultas/controller/EspecialidadeController.java
+++ src/main/java/com/fiap/backend_consultas/controller/EspecialidadeController.java
@@ -1,34 +1,34 @@
 package com.fiap.backend_consultas.controller;
+
 import com.fiap.backend_consultas.model.Especialidade;
 import com.fiap.backend_consultas.service.EspecialidadeService;
 import org.springframework.web.bind.annotation.*;
-        import java.util.List;
+
+import java.util.List;
+
 @RestController
 @RequestMapping("/especialidades")
 @CrossOrigin
 public class EspecialidadeController {
-    private final EspecialidadeService service;
-    public EspecialidadeController(EspecialidadeService service) {
-        this.service = service;
-    }
-    @PostMapping
-    public Especialidade criar(@RequestBody Especialidade especialidade) {
-        return service.salvar(especialidade);
-    }
-    @GetMapping
-    public List<Especialidade> listar() {
-        return service.listar();
-    }
-    @GetMapping("/{id}")
-    public Especialidade buscarPorID(@PathVariable Long id){
-        return service.buscarPorId(id);
-    }
-    @PutMapping("/{id}")
-    public  Especialidade atualizar(@PathVariable Long id,@RequestBody Especialidade especialidade){
-        return service.atualizar(id, especialidade);
-    }
-    @DeleteMapping("/{id}")
-    public void deletar(@PathVariable Long id){
-        service.deletar(id);
-    }
+
+ private final EspecialidadeService service;
+
+ public EspecialidadeController(EspecialidadeService service) {
+ this.service = service;
+ }
+
+ @PostMapping
+ public Especialidade criar(@RequestBody Especialidade especialidade) {
+ return service.salvar(especialidade);
+ }
+
+ @GetMapping
+ public List<Especialidade> listar() {
+ return service.listar();
+ }
+
+ @GetMapping("/{id}")
+ public Especialidade buscarPorId(@PathVariable Long id) {
+ return service.buscarPorId(id);
+ }
 }
--- src/main/java/com/fiap/backend_consultas/controller/MedicoController.java
+++ src/main/java/com/fiap/backend_consultas/controller/MedicoController.java
@@ -4,40 +4,41 @@ import com.fiap.backend_consultas.model.Medico;
 import com.fiap.backend_consultas.service.MedicoService;
 import org.springframework.web.bind.annotation.*;
 
-        import java.util.List;
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
+@CrossOrigin
 public class MedicoController {
 
-    private final MedicoService service;
+ private final MedicoService service;
 
-    public MedicoController(MedicoService service) {
-        this.service = service;
-    }
+ public MedicoController(MedicoService service) {
+ this.service = service;
+ }
 
-    @GetMapping
-    public List<Medico> listar() {
-        return service.listar();
-    }
+ @GetMapping
+ public List<Medico> listar() {
+ return service.listar();
+ }
 
-    @GetMapping("/{id}")
-    public Medico buscarPorId(@PathVariable Long id) {
-        return service.buscarPorId(id);
-    }
+ @GetMapping("/{id}")
+ public Medico buscarPorId(@PathVariable Long id) {
+ return service.buscarPorId(id);
+ }
 
-    @PostMapping
-    public Medico salvar(@RequestBody Medico medico) {
-        return service.salvar(medico);
-    }
+ @PostMapping
+ public Medico salvar(@RequestBody Medico medico) {
+ return service.salvar(medico);
+ }
 
-    @PutMapping("/{id}")
-    public Medico atualizar(@PathVariable Long id, @RequestBody Medico medico) {
-        return service.atualizar(id, medico);
-    }
+ @PutMapping("/{id}")
+ public Medico atualizar(@PathVariable Long id, @RequestBody Medico medico) {
+ return service.atualizar(id, medico);
+ }
 
-    @DeleteMapping("/{id}")
-    public void deletar(@PathVariable Long id) {
-        service.deletar(id);
-    }
+ @DeleteMapping("/{id}")
+ public void deletar(@PathVariable Long id) {
+ service.deletar(id);
+ }
 }
